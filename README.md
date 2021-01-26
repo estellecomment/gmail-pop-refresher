bce32c17281377f8f17a0fb810a99923cda94961
Estelle Comment <estelle.comment@gmail.com>
2021-01-26 16:25:22 +0100

    More readme, restore code that got lost

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # gmail-pop-refresher
-You use Gmail on desktop as a POP client, and you want to control the email fetching frequency.
+You use Gmail on desktop as a POP client, and you want to control the email fetching frequency. This extension clicks the button on the Accounts page to fetch mail.
+
+To make it work, go to the accounts page : https://mail.google.com/mail/u/0/#settings/accounts
+You will see "Checking mail..." every 10 seconds.
+
+You need to leave the page open in a tab for the refresh to work.
 
 ![Screenshot](screenshot.png)
--- refresh.js
+++ refresh.js
@@ -1,12 +1,15 @@
-{
-  "name": "Gmail POP refresher",
-  "version": "1.0",
-  "description": "You use Gmail on desktop as a POP client, and you want to control the email fetching frequency.",
-  "manifest_version": 2,
-  "content_scripts": [
-    {
-      "matches": ["https://mail.google.com/*"],
-      "js": ["refresh.js"]
-    }
-  ]
+function refreshPop(){
+  var $el = document.getElementsByClassName("rP sA");
+  if ($el.length > 0){
+    console.log('Clicking "Check mail now" button to fetch mail.')
+    $el = $el[0];
+    $el.click();
+  } else {
+    console.log('[Gmail Pop refresher] "Check mail now" button not found. Mail not fetched. (Maybe the Gmail page has not finished loading?')
+  }
+  setTimeout(refreshPop, 10000)
 }
+
+console.log('[Gmail Pop refresher] Running Gmail Pop reresher')
+console.log('[Gmail Pop refresher] For this extension to work, you should be on the Settings page, in Accounts, something like : https://mail.google.com/mail/u/0/#settings/accounts')
+refreshPop()
